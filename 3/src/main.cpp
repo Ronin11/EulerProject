d1358e696bfdb9f4f09636e751d99ae43d880838
Ronin11 <moduledatacollector@gmail.com>
2015-02-20 16:05:30 -0700

    3 Optimizations

--- 3/src/main.cpp
+++ 3/src/main.cpp
@@ -10,49 +10,31 @@
 
 const unsigned long int TARGET = 600851475143;
 
-//Check if integer is prime. Optimized with the list of primes,
-//All non-prime numbers, must be divisible by a prime number, and 
-// will always be divisible by a number less than itself.
-bool is_Prime(unsigned long int *prime, std::vector<unsigned long int> *list){
-	for(int i = 0; i < list->size(); i++){
-		if(*prime % list->operator[](i) == 0)
+//Check if integer is prime.
+bool is_Prime(unsigned long int *prime){
+	if(*prime % 2 == 0)
+		return false;
+	for(int i = 3; i < *prime/2; i+=2){
+		if(*prime % i == 0)
 			return false;
 	}
 	return true;
 }
 
-//Iterate until the target is met.
-std::vector<unsigned long int> * generate_Primes(){
-	static std::vector<unsigned long int> data = std::vector<unsigned long int>();
-	data.reserve(TARGET/5);
-	data.push_back(2);//Seed the vector with the first prime number.
-	for(unsigned long int i = 3; i <= TARGET/2; i+=2)
-		if(is_Prime(&i,&data))
-			data.push_back(i);
-	return &data;
-}
-
-//Find the Prime Factors
-unsigned long int get_Prime_Factors(std::vector<unsigned long int> * primes){
-	//std::vector<unsigned long int> factors = std::vector<unsigned long int>();
-	for(int i = primes->size()-1; i >= 0; i--)
-		if(TARGET % primes->operator[](i) == 0)
-			return primes->operator[](i);
-			//factors.push_back(primes[i]);
-	//return factors;
+//Find the Largest Prime Factor
+unsigned long int get_Largest_Prime_Factor(){
+	for(unsigned long int i = TARGET/2; i >= 3; i-=2){//We don't need to iterate through the even numbers.
+		if(i % 2 == 0)
+			--i; //Make sure we are iterating through odd numbers.
+		if(TARGET % i == 0)
+			if(is_Prime(&i))
+				return i;
+		}
 }
 
 int main(void){
-	auto start = std::chrono::system_clock::now();
-	std::vector<unsigned long int> *numbers = generate_Primes();
-	unsigned long int factor = get_Prime_Factors(numbers);
-	auto stop = std::chrono::system_clock::now();
-	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count();
-	std::cout << "Time Taken: " << duration << " microseconds." << std::endl;
-	//std::cout <<  "Factors of " << TARGET << ": ";
-	//for(int i = 0; i < factors.size(); i++)
-	//	std::cout <<  factors[i] << " ";
-	//std::cout << std::endl;
+
+	unsigned long int factor = get_Largest_Prime_Factor();
 	std::cout << "The answer is: " << factor << std::endl;
 	return 0;
 
